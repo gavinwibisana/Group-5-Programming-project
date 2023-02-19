4f6bf4775dc4825c47bf5cc822c3d97b1c256114
Gavin Wibisana <gavinwibisana@yahoo.co.id>
2023-02-19 10:28:37 +0800

    Create main.c
    create main.c, add group member list

---
+++ main.c
@@ -0,0 +1,9 @@
+/* 
+CRM fundamentals programmming Group Project
+members:
+Gavin Wibisana 407NKTOB
+Kaiwen Xue 
+Okkar Sett DEET 6879ES6V
+Yuehaozhe Xie 926qraeo
+GitHub Link: https://github.com/gavinwibisana/Group-5-Programming-project
+*/
