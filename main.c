92f1852562b67d02b77807f45e251bdcfea38f31
Gavin Wibisana <gavinwibisana@yahoo.co.id>
2023-02-19 11:20:47 +0800

    Update main.c
    Add Main Menu

--- main.c
+++ main.c
@@ -7,3 +7,16 @@ Okkar Sett DEET 6879ES6V
 Yuehaozhe Xie 926qraeo
 GitHub Link: https://github.com/gavinwibisana/Group-5-Programming-project
 */
+
+#include <stdio.h>
+
+int main(){
+	int inputMenu = 0;
+		printf("-------------- MAIN MENU ------------\n");
+        printf("1. Contact Management \n");
+        printf("2. Sales Data \n");
+        printf("3. Exit \n");
+        printf("Enter your Choice: \n");
+        scanf("%d", &inputMenu);
+	return 0;
+}
